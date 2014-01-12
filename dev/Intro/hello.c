bebbb3909ad2d449fdda10c8d1a3984df484433a
stevenjens <stevenjensj@gmail.com>
2014-01-12 15:34:09 -0500

    add dev environment from class plus hw1

---
+++ dev/Intro/hello.c
@@ -0,0 +1,12 @@
+/*
+PROG: hello
+LANG: C
+*/
+#include <stdio.h>
+
+int main(void){
+  FILE *fout = fopen( "hello.out", "w");
+  fprintf( fout, "Hello, world!\n" );
+  fclose( fout);
+  return 0;
+ }
